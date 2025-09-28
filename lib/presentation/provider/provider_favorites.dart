e1d4ed491880141429588aacb58d63725ea38058
andres martinez <amartinez@loyaltysci.com>
2025-09-28 18:51:02 -0500

    feat(search,favorites)
    ajuste búsqueda y personajes favoritos.

--- lib/presentation/provider/provider_favorites.dart
+++ lib/presentation/provider/provider_favorites.dart
@@ -1,5 +1,45 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:repaso/domain/entities/entities_people.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProviderFavorites extends ChangeNotifier{
+  final List<Result> _favorites = [];
+
+  List<Result> get favorites => List.unmodifiable(_favorites);
+
+  ProviderFavorites(){
+    _loadFromPrefer();
+  }
+
+ bool isFavorite(Result people) {
+   return _favorites.any((b) => b.id == people.id);
+ }
+
+  void toogleFavorite(Result people) {
+    if(isFavorite(people)){
+      _favorites.removeWhere((b) => b.id == people.id);
+    } else {
+      _favorites.add(people);
+    }
+    notifyListeners();
+    _saveToPrefs();
+  }
+
+  Future<void> _saveToPrefs() async{
+    final prefer = await SharedPreferences.getInstance();
+    final list = _favorites.map((b) => jsonEncode(b.toJson())).toList();
+    await prefer.setStringList("favorites", list);
+
+  }
+
+  Future<void> _loadFromPrefer() async {
+    final prefer = await SharedPreferences.getInstance();
+    final list = prefer.getStringList("favorites") ?? [];
+    _favorites.clear();
+    _favorites.addAll(list.map((str) => Result.fromJson(jsonDecode(str))));
+    notifyListeners();
+  }
 
 }
--- lib/presentation/widgets/home.dart
+++ lib/presentation/widgets/home.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:repaso/domain/entities/entities_people.dart'as model;
 import 'package:repaso/presentation/provider/provider_Home.dart';
+import 'package:repaso/presentation/provider/provider_favorites.dart';
 import 'package:repaso/presentation/widgets/description_person.dart';
 
 class Home extends StatefulWidget {
@@ -14,6 +15,9 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   int _index = 0;
+  bool _search = false;
+  String _query = "";
+  final _controller = TextEditingController();
 
   @override
   void initState() {
@@ -23,16 +27,36 @@ class _HomeState extends State<Home> {
     });
   }
 
-  void _descriptionPerson(model.Result person){
-    Navigator.push(context, MaterialPageRoute(builder: (context) => DescriptionPerson(dataPerson: person)));
+  void _selectSearch(){
+    setState(() => _search = !_search);
   }
+
 
 
   @override
   Widget build(BuildContext context) {
     final providerPeople = Provider.of<ProviderHome>(context);
+    final favorites = Provider.of<ProviderFavorites>(context);
     final error = providerPeople.error;
     final peopleData = providerPeople.people;
+
+
+  final filtter =
+      peopleData!.results.where((b) {
+        final q = _query.trim().toLowerCase();
+        if (q.isEmpty) return true;
+        final data = b.name.toLowerCase();
+        return data.contains(q);
+      }).toList();
+
+  final filtterFavorites =
+      favorites.favorites.where((b) {
+        final q = _query.trim().toLowerCase();
+        if(q.isEmpty) return true;
+        final data = b.name.toLowerCase();
+        return data.contains(q);
+      }).toList();
+
 
     final pages = [
       if (error != null)
@@ -41,10 +65,11 @@ class _HomeState extends State<Home> {
         const Center(child: CircularProgressIndicator())
       else
         ListView.builder(
-          itemCount: peopleData.results.length,
+          itemCount: filtter.length,
           itemBuilder: (context, index) {
-            final person = peopleData.results[index];
-            return GestureDetector(
+            final person = filtter[index];
+            return _CardView(person);
+           /* return GestureDetector(
               onTap: () => _descriptionPerson(person),
               child: ListTile(
                   leading: Image.network(
@@ -57,17 +82,64 @@ class _HomeState extends State<Home> {
                   subtitle: Text(person.status),
                   trailing: Text(person.species),
                 ),
-            );
-
+            );*/
           },
         ),
-      const Center(child: Text("Favoritos")),
+      favorites.favorites.isEmpty
+      ? const Center(child: Text("AUN NO TIENES FAVORITOS"),)
+     : ListView.builder(
+        itemCount: filtterFavorites.length,
+          itemBuilder: (context, index) {
+          final person = filtterFavorites[index];
+          return _CardView(person);
+          }
+      ),
+
+      //const Center(child: Text()),
     ];
 
     return Scaffold(
       appBar: AppBar(
-        title: Text("RICK AND MORTY", style: GoogleFonts.adamina(color: Colors.white)),
-        backgroundColor: Colors.green,
+          backgroundColor: Colors.green,
+          toolbarHeight: 75,
+          actions: [IconButton(onPressed: _selectSearch,icon: Icon(Icons.search),color: Colors.white)],
+        title: !_search
+        ? Text("RICK AND MORTY", style: GoogleFonts.adamina(color: Colors.white))
+        : TextField(
+          controller: _controller,
+          onChanged: (value) => setState(() =>  _query = value),
+          textInputAction: TextInputAction.search,
+          style: TextStyle(color: Colors.white),
+          cursorColor: Colors.white,
+          decoration: InputDecoration(
+            isDense: true,
+            hintText: "Buscar",
+            hintStyle: TextStyle(color: Colors.white),
+            prefixIcon: Icon(Icons.search, color: Colors.white),
+            suffixIcon:
+              _query.isNotEmpty
+              ? IconButton(
+                icon: Icon(Icons.clear, color: Colors.white),
+                onPressed: () {
+                  _controller.clear();
+                  setState(() => _query = "");
+                },
+              )
+                  : null,
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(12),
+              borderSide: BorderSide(color: Colors.white)
+            )
+          ),
+
+
+        ),
+       /* ),
+        ),*/
       ),
       body: SafeArea(child: pages[_index]),
       bottomNavigationBar: NavigationBar(
@@ -77,6 +149,76 @@ class _HomeState extends State<Home> {
           NavigationDestination(icon: Icon(Icons.list), label: "Lista"),
           NavigationDestination(icon: Icon(Icons.stars), label: "Favoritos"),
         ],
+      ),
+    );
+  }
+}
+
+class _CardView extends StatefulWidget {
+  final model.Result datos;
+
+
+  const _CardView(this.datos);
+
+  @override
+  State<_CardView> createState() => _CardViewState();
+}
+
+class _CardViewState extends State<_CardView> {
+
+
+  void _descriptionPerson(model.Result person){
+    Navigator.push(context, MaterialPageRoute(builder: (context) => DescriptionPerson(dataPerson: person)));
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    final favorites = Provider.of<ProviderFavorites>(context);
+    final isFav = favorites.isFavorite(widget.datos);
+
+
+    return GestureDetector(
+      onTap: () => _descriptionPerson(widget.datos),
+      child: Card(
+        elevation: 4,
+          margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        child: Padding(padding: EdgeInsets.all(10),
+        child:  Row(
+            children: [
+              ClipRect(
+                child: Image.network(
+                  widget.datos.image,
+                  height: 60,
+                  width: 60,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(width: 20),
+              Expanded(child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(widget.datos.name ?? ""),
+                Text(widget.datos.species ?? "")
+              ],
+              ),
+              ),
+              SizedBox(width: 40),
+              Column(
+                children: [
+                  FilledButton(onPressed: () => favorites.toogleFavorite(widget.datos),
+                      style: FilledButton.styleFrom(backgroundColor: Colors.green
+                      ),
+                       child: Icon(
+                      isFav ? Icons.star_outlined : Icons.star_border,
+                      color: isFav ? Colors.yellow : Colors.yellow,
+                    ),
+                  )
+                ],
+              )
+            ],
+        )),
       ),
     );
   }
